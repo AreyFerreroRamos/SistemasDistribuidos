7b9f395564845754bf0b3854cd17d9c0a8773a01
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-07-13 15:14:40 +0200

    Arey: Primera versió de l'arquitectura directa amb gRPC i XMLRPC. Lectura de fitxer correcta.

--- Practica1/DirectCommunicationXMLRPCgRPC/client.py
+++ Practica1/DirectCommunicationXMLRPCgRPC/client.py
@@ -1,22 +1,24 @@
 import xmlrpc.client
 import sys
 
+import grpc
+import daskFunctions_pb2
+import daskFunctions_pb2_grpc
+
 client_master = xmlrpc.client.ServerProxy('http://localhost:9000')
 
-maxs=[]
-mins=[]
+#maxs=[]
+#mins=[]
 i=1
 
 while i<len(sys.argv):
-    client_worker = xmlrpc.client.ServerProxy(client_master.listWorkers()[(i-1)%client_master.numWorkers()])
-    print(client_worker.readCSV(sys.argv[i])+"\n")
-    print(client_worker.columns()+"\n")
-    print(client_worker.head(5)+"\n")
-    print(client_worker.isin('City', 'Tarragona')+"\n")
-    print(client_worker.item(5, 3)+"\n")
-    maxs.append(float(client_worker.max('Temp_max')))
-    mins.append(float(client_worker.min('Temp_min')))
+    channel = grpc.insecure_channel('localhost:'+client_master.listWorkers()[(i-1)%client_master.numWorkers()].split(':')[2])
+    client_worker = daskFunctions_pb2_grpc.DaskFunctionsStub(channel)
+    name_file = daskFunctions_pb2.NameFile(value=sys.argv[i])
+    print(client_worker.ReadCSV(name_file).value+"\n")
+    #maxs.append(float(client_worker.Max(field).value))
+    #mins.append(float(client_worker.min('Temp_min')))
     i+=1
 
-print("Temperatura maxima: "+str(max(maxs)))
-print("Temperatura minima: "+str(min(mins)))
+#print("Temperatura maxima: "+str(max(maxs)))
+#print("Temperatura minima: "+str(min(mins)))
--- Practica1/DirectCommunicationXMLRPCgRPC/worker.py
+++ Practica1/DirectCommunicationXMLRPCgRPC/worker.py
@@ -2,21 +2,35 @@ import xmlrpc.client
 import sys
 import os
 
-from xmlrpc.server import SimpleXMLRPCServer
-import logging
+import grpc
+from concurrent import futures
+import time
+
+import daskFunctions_pb2
+import daskFunctions_pb2_grpc
+
 import daskFunctions
 
 proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
 proxy.addWorker('http://localhost:'+sys.argv[1])
 
-worker = SimpleXMLRPCServer(('localhost', int(sys.argv[1])), logRequests=True)
-logging.basicConfig(level=logging.INFO)
+class DaskFunctionsServicer(daskFunctions_pb2_grpc.DaskFunctionsServicer):
+    def ReadCSV(self, request, context):
+        response = daskFunctions_pb2.NameFile()
+        response.value = daskFunctions.DaskFunctions().readCSV(request.value)
+        return response
 
-worker.register_instance(daskFunctions.DaskFunctions())
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+daskFunctions_pb2_grpc.add_DaskFunctionsServicer_to_server(DaskFunctionsServicer(), server)
+
+print('Starting server. Listening on port '+sys.argv[1]+'.')
+server.add_insecure_port('[::]:'+sys.argv[1])
+server.start()
 
 try:
-    print('Use control + c to exit the Worker node')
-    worker.serve_forever()
+    while True:
+        time.sleep(86400)
 except KeyboardInterrupt:
-    print('Exiting Worker node')
+    server.stop(0)
     proxy.removeWorker(sys.argv[1])
