6d7fb482e3846a3dd24dd265c6c044b97f7acd9b
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-07-18 21:16:56 +0200

    Arey: Addició del mètode columns.

--- Practica1/IndirectCommunicationRedis/client.py
+++ Practica1/IndirectCommunicationRedis/client.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 redis_cli = redis.Redis(host="localhost", port=16379, decode_responses=True, encoding="utf-8")
-pubsub_read_file = redis_cli.pubsub()
+pubsub = redis_cli.pubsub()
 
 maxs=[]
 mins=[]
@@ -11,15 +11,16 @@ i=1
 
 while i<len(sys.argv):
     redis_cli.publish('worker'+str(i%int(redis_cli.get('num_workers'))), sys.argv[i])
-    pubsub_read_file.subscribe(sys.argv[i])
+    pubsub.subscribe(sys.argv[i])
     capturate=False
     while not capturate:
-        message = pubsub_read_file.get_message(ignore_subscribe_messages=True)
+        message = pubsub.get_message(ignore_subscribe_messages=True)
         if message and (message.get('type') == 'message'):
             print(message.get('data')+"\n")
-            maxs.append(float(pubsub_read_file.get_message(ignore_subscribe_messages=True).get('data')))
-            mins.append(float(pubsub_read_file.get_message(ignore_subscribe_messages=True).get('data')))
-            pubsub_read_file.unsubscribe(sys.argv[i])
+            print(pubsub.get_message(ignore_subscribe_messages=True).get('data')+"\n")
+            maxs.append(float(pubsub.get_message(ignore_subscribe_messages=True).get('data')))
+            mins.append(float(pubsub.get_message(ignore_subscribe_messages=True).get('data')))
+            pubsub.unsubscribe(sys.argv[i])
             capturate = True
         else:
             time.sleep(1)
--- Practica1/IndirectCommunicationRedis/worker.py
+++ Practica1/IndirectCommunicationRedis/worker.py
@@ -21,6 +21,7 @@ try:
         message = pubsub_name_file.get_message(ignore_subscribe_messages=True)
         if message and (message.get('type') == "message"):
             redis_cli.publish(message.get('data'), worker.readCSV(message.get('data')))
+            redis_cli.publish(message.get('data'), worker.columns())
             redis_cli.publish(message.get('data'), worker.max('Temp_max'))
             redis_cli.publish(message.get('data'), worker.min('Temp_min'))
         message_restructure = pubsub_restructure_nodes.get_message(ignore_subscribe_messages=True)
