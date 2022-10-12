1954b5fe8d57ef90a54b9716bef1a9808f81a6d9
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-10-12 17:59:57 +0200

    Arey: Primer paso de paralelización de la arquitectura de comunicación directa con middleware XMLRPC.

--- Task1/DirectCommunicationXMLRPC/client.py
+++ Task1/DirectCommunicationXMLRPC/client.py
@@ -1,13 +1,9 @@
 import xmlrpc.client
+import threading
 import sys
 
-client_master = xmlrpc.client.ServerProxy('http://localhost:9000')
-
-maxs=[]
-mins=[]
-i=1
-
-while i<len(sys.argv):
+def treat_file(maxs, mins, i):
+    client_master = xmlrpc.client.ServerProxy('http://localhost:9000')
     client_worker = xmlrpc.client.ServerProxy(client_master.listWorkers()[(i-1)%client_master.numWorkers()])
     print(client_worker.readCSV(sys.argv[i])+"\n")
     print(client_worker.columns()+"\n")
@@ -16,6 +12,20 @@ while i<len(sys.argv):
     print(client_worker.item(5, 3)+"\n")
     maxs.append(float(client_worker.max('Temp_max')))
     mins.append(float(client_worker.min('Temp_min')))
+
+threads=[]
+maxs=[]
+mins=[]
+i=1
+
+while i<len(sys.argv):
+    threads.append(threading.Thread(target=treat_file, name="thread%s" %i, args=(maxs, mins, i)))
+    threads[i-1].start()
+    i+=1
+
+i=1
+while i<len(sys.argv):
+    threads[i-1].join()
     i+=1
 
 print("Temperatura maxima: "+str(max(maxs)))
