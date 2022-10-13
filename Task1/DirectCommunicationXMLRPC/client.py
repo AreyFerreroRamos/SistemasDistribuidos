38bc7863d201953dc923240f0589b866f9c5338c
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-10-13 19:23:32 +0200

    Arey: Paralelización de la arquitectura de comunicación directa con XMLRPC funcional.

--- Task1/DirectCommunicationXMLRPC/client.py
+++ Task1/DirectCommunicationXMLRPC/client.py
@@ -2,8 +2,8 @@ import xmlrpc.client
 import threading
 import sys
 
-def treat_file(client_worker, num_thread, maxs, mins):
-    print(client_worker.readCSV(sys.argv[num_thread])+"\n")
+def treat_file(client_worker, i, maxs, mins):
+    print(client_worker.readCSV(sys.argv[i])+"\n")
     print(client_worker.columns()+"\n")
     print(client_worker.head(5)+"\n")
     print(client_worker.isin('City', 'Tarragona')+"\n")
@@ -11,22 +11,26 @@ def treat_file(client_worker, num_thread, maxs, mins):
     maxs.append(float(client_worker.max('Temp_max')))
     mins.append(float(client_worker.min('Temp_min')))
 
+client_master = xmlrpc.client.ServerProxy('http://localhost:9000')
+
 threads=[]
 maxs=[]
 mins=[]
+i=1
 
-num_thread=1
-while num_thread<len(sys.argv):
-    client_master = xmlrpc.client.ServerProxy('http://localhost:9000')
-    client_worker = xmlrpc.client.ServerProxy(client_master.listWorkers()[(num_thread-1)%client_master.numWorkers()])
-    threads.append(threading.Thread(target=treat_file, name="thread%s" %(num_thread+1), args=(client_worker, num_thread, maxs, mins)))
-    threads[num_thread-1].start()
-    num_thread+=1
-
-num_thread=1
-while num_thread<len(sys.argv):
-    threads[num_thread-1].join()
-    num_thread+=1
+while i<len(sys.argv):
+    num_worker=0
+    while (num_worker<client_master.numWorkers() and (i<len(sys.argv))):
+        client_worker = xmlrpc.client.ServerProxy(client_master.listWorkers()[num_worker])
+        threads.append(threading.Thread(target=treat_file, name="thread%s" %i, args=(client_worker, i, maxs, mins)))
+        threads[num_worker].start()
+        num_worker+=1
+        i+=1
+    num_worker=0
+    while (num_worker<len(threads)):
+        threads[num_worker].join()
+        num_worker+=1
+    threads.clear()
 
 print("Temperatura maxima: "+str(max(maxs)))
 print("Temperatura minima: "+str(min(mins)))
