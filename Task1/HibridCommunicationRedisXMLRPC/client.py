95007705e3c5eec04d1d3185253da2aa8943888f
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-10-13 21:46:56 +0200

    Arey: Paralelización de la arquitectura de comunicación híbrida con XMLRPC y Redis completamente funcional.

--- Task1/HibridCommunicationRedisXMLRPC/client.py
+++ Task1/HibridCommunicationRedisXMLRPC/client.py
@@ -1,17 +1,11 @@
 import redis
 import xmlrpc.client
+import threading
 import sys
 
 redis_cli = redis.Redis(host="localhost", port=16379, decode_responses=True, encoding="utf-8")
 
-maxs=[]
-mins=[]
-i=1
-
-while i<len(sys.argv):
-    worker = redis_cli.lpop('workers')
-    client_worker = xmlrpc.client.ServerProxy(worker)
-    redis_cli.rpush('workers', worker)
+def treat_file(client_worker, i, max, mins):
     print(client_worker.readCSV(sys.argv[i])+"\n")
     print(client_worker.columns()+"\n")
     print(client_worker.head(5)+"\n")
@@ -19,7 +13,27 @@ while i<len(sys.argv):
     print(client_worker.item(5, 3)+"\n")
     maxs.append(float(client_worker.max('Temp_max')))
     mins.append(float(client_worker.min('Temp_min')))
-    i+=1
+
+threads=[]
+maxs=[]
+mins=[]
+
+i=1
+while i<len(sys.argv):
+    num_worker=0
+    while ((num_worker<redis_cli.llen('workers')) and (i<len(sys.argv))):
+        worker = redis_cli.lpop('workers')
+        client_worker = xmlrpc.client.ServerProxy(worker)
+        threads.append(threading.Thread(target=treat_file, name="thread%s" %i, args=(client_worker, i, maxs, mins)))
+        threads[num_worker].start()
+        num_worker+=1
+        i+=1
+        redis_cli.rpush('workers', worker)
+    num_worker=0
+    while num_worker<len(threads):
+        threads[num_worker].join()
+        num_worker+=1
+    threads.clear()
 
 print("Temperatura maxima: "+str(max(maxs)))
 print("Temperatura minima: "+str(min(mins)))
