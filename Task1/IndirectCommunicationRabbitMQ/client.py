709939cb8a9152f5f3b97329e16d497faf7adaa1
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-10-21 17:23:52 +0200

    Arey: Reestructuración del código en forma descendente.

--- Task1/IndirectCommunicationRabbitMQ/client.py
+++ Task1/IndirectCommunicationRabbitMQ/client.py
@@ -1,26 +1,7 @@
-import pika
+import publisher
 import sys
-
-class Publisher:
-    def __init__(self, config):
-        self.config = config
-        self.connection = self.create_connection()
-
-    def __del__(self):
-        self.connection.close()
-
-    def create_connection(self):
-        return pika.BlockingConnection(pika.ConnectionParameters(host=self.config['host'], port=self.config['port']))
-
-    def publish(self, routing_key, message):
-        channel = self.connection.channel()
-
-        channel.exchange_declare(exchange='proves', exchange_type='topic')
-
-        channel.basic_publish(exchange='proves', routing_key=routing_key, body=message)
-        print(" [x] Sent message %r from %r" % (message, routing_key))
         
-publisher_name = Publisher({'host': 'localhost', 'port': 5672})
+publisher_name = publisher.Publisher({'host': 'localhost', 'port': 5672})
 
 #maxs=[]
 #mins=[]
---
+++ Task1/IndirectCommunicationRabbitMQ/consumer.py
@@ -0,0 +1,33 @@
+import pika
+
+class Consumer:
+    def __init__(self, config, queue_name, binding_key):
+        self.config = config
+        self.queue_name = queue_name
+        self.binding_key = binding_key
+        self.connection = self.create_connection()
+
+    def __del__(self):
+        self.connection.close()
+
+    def create_connection(self):
+        return pika.BlockingConnection(pika.ConnectionParameters(host=self.config['host'], port=self.config['port']))
+
+    def callback(self, channel, method, properties, body):
+        print(" [x] Received new message %r from %r" % (body, method.routing_key))
+
+    def consume(self):
+        channel = self.connection.channel()
+
+        channel.exchange_declare(exchange='proves', exchange_type='topic')
+        channel.queue_declare(queue=self.queue_name, exclusive=True)
+        channel.queue_bind(exchange='proves', queue=self.queue_name, routing_key=self.binding_key)
+
+        channel.basic_consume(queue=self.queue_name, on_message_callback=self.callback, auto_ack=True)
+
+        try:
+            print(' [*] Waiting for data for '+self.queue_name+'. Use control + c to exit the Worker node')
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            print(' [*] Exiting Worker node')
+            channel.stop_consuming()
