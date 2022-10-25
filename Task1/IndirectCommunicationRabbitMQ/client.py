1e576786d48e67ac5e124281e27d8d0597eef435
AreyFerreroRamos <arey.ferrero@estudiants.urv.cat>
2022-10-25 16:28:56 +0200

    Arey: Reestructuración del código. Eliminación del diseño descendente.

--- Task1/IndirectCommunicationRabbitMQ/client.py
+++ Task1/IndirectCommunicationRabbitMQ/client.py
@@ -1,7 +1,26 @@
-import publisher
+import pika
 import sys
+
+class Publisher:
+    def __init__(self, config):
+        self.config = config
+        self.connection = self.create_connection()
+
+    def __del__(self):
+        self.connection.close()
+
+    def create_connection(self):
+        return pika.BlockingConnection(pika.ConnectionParameters(host=self.config['host'], port=self.config['port']))
+
+    def publish(self, routing_key, message):
+        channel = self.connection.channel()
+
+        channel.exchange_declare(exchange='proves', exchange_type='topic')
+
+        channel.basic_publish(exchange='proves', routing_key=routing_key, body=message)
+        print(" [x] Sent message %r from %r" % (message, routing_key))
         
-publisher_name = publisher.Publisher({'host': 'localhost', 'port': 5672})
+publisher_name = Publisher({'host': 'localhost', 'port': 5672})
 
 #maxs=[]
 #mins=[]
