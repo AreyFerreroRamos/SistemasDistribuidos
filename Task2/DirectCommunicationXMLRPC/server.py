c321936c74a3e414a96bdad719116c759fc61e66
Arey Ferrero Ramos <arey.ferrero2@estudiants.urv.cat>
2022-12-12 18:47:57 +0100

    Arey: Borrado de código repetido.

--- Task2/DirectCommunicationXMLRPC/server.py
+++ Task2/DirectCommunicationXMLRPC/server.py
@@ -5,29 +5,24 @@ import sys
 import workerFunctions
 import daskFunctions
 
+logging.basicConfig(level=logging.INFO)
+
 if (len(sys.argv) == 1):
-    master = SimpleXMLRPCServer(('localhost', 9000), logRequests=True)
-    logging.basicConfig(level=logging.INFO)
-
-    master.register_instance(workerFunctions.WorkerFunctions())
-
-    try:
-        print('Use control + c to exit the Master node')
-        master.serve_forever()
-    except KeyboardInterrupt:
-        print('Exiting master node')
+    server = SimpleXMLRPCServer(('localhost', 9000), logRequests=True)
+    server.register_instance(workerFunctions.WorkerFunctions())
+    node="master"
 else:
     proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
     proxy.addWorker('http://localhost:'+sys.argv[1])
 
-    worker = SimpleXMLRPCServer(('localhost', int(sys.argv[1])), logRequests=True)
-    logging.basicConfig(level=logging.INFO)
+    server = SimpleXMLRPCServer(('localhost', int(sys.argv[1])), logRequests=True)
+    server.register_instance(daskFunctions.DaskFunctions())
+    node="worker"
 
-    worker.register_instance(daskFunctions.DaskFunctions())
-
-    try:
-        print('Use control + c to exit the Worker node')
-        worker.serve_forever()
-    except KeyboardInterrupt:
-        print('Exiting Worker node')
+try:
+    print('Use control + c to exit the '+node+' node.')
+    server.serve_forever()
+except KeyboardInterrupt:
+    print('Exiting '+node+' node...')
+    if (len(sys.argv) == 2):
         proxy.removeWorker(sys.argv[1])
