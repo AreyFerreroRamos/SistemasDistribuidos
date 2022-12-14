0189c1259c98361d574c18983dba88cf435c7f87
Arey Ferrero Ramos <arey.ferrero2@estudiants.urv.cat>
2022-12-14 19:50:35 +0100

    Arey: Implementación y tratamiento de un hilo de ejecución (thread). Desde dicho hilo se enviarán los pings a otro nodo.

--- Task2/DirectCommunicationXMLRPC/server.py
+++ Task2/DirectCommunicationXMLRPC/server.py
@@ -1,8 +1,19 @@
 from xmlrpc.server import SimpleXMLRPCServer
 import xmlrpc.client
+import threading
 import logging
+import time
 import sys
+import os
 import serverFunctions
+
+def treat_ping():
+    while True:
+        # Pings.
+        if event.is_set():
+            break
+        else:
+            time.sleep(1)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,8 +31,13 @@ server.register_instance(serverFunctions.ServerFunctions())
 
 try:
     print('Use control + c to exit the '+node+' node.')
+    name_thread = threading.Thread(target=treat_ping)
+    event = threading.Event()
+    name_thread.start()
     server.serve_forever()
 except KeyboardInterrupt:
     print('Exiting '+node+' node...')
+    event.set()
+    name_thread.join()
     if (len(sys.argv) == 2):
         proxy.removeWorker(sys.argv[1])
