86a5a95586d120b68d3f08e038bb656e094417d2
Arey Ferrero Ramos <arey.ferrero2@estudiants.urv.cat>
2022-12-16 19:50:06 +0100

    Arey: Unificación de las clases workerManagerFunctions.py y clientFunctions.py en una única clase llamada workerFunctions.py. Esto permite que todos los nodos tengan acceso a las funciones para gestionar los workers, algo que será necesario para llevar a cabo el algoritmo de elección de líder.

--- Task2/DirectCommunicationXMLRPC/server.py
+++ Task2/DirectCommunicationXMLRPC/server.py
@@ -6,29 +6,26 @@ import time
 import sys
 import os
 import masterManagerFunctions
-import workersManagerFunctions
-import clientFunctions
+import workerFunctions
 
 #def treat_ping_master(server):
- #   while True:
-  #      #Pings.
-   #     if event.is_set():
-    #        break
-     #   else:
-      #      time.sleep(1)
-
-#def treat_ping_worker(server):
  #   while True:
   #      if event.is_set():
    #         break
     #    else:
-     #       master = server.getMaster()
-      #      response = os.system("ping -c 1 "+master)
-       #     if (response != 0):
-        #        server.addMaster(server.getWorker())
-         #   else:
-          #      server.addMaster(master)
-           # time.sleep(1)
+     #       print("Ping")
+      #  time.sleep(1)
+
+#def treat_ping_worker(masterManager, server):
+ #   while True:
+  #      if event.is_set():
+   #         break
+    #    else:
+     #       response = os.system("ping -c 1 "+masterManager.getMaster())
+      #      if (response != 0):
+       #         server.removeWorker(sys.argv[1])
+        #        masterManager.setMaster('http://localhost:'+sys.argv[1])
+        #time.sleep(1)
 
 masterManager = masterManagerFunctions.MasterManagerFunctions()
 logging.basicConfig(level=logging.INFO)
@@ -39,9 +36,8 @@ if (len(sys.argv) == 1):
     masterManager.setMaster('http://localhost:9000')
     
     server = SimpleXMLRPCServer(('localhost', 9000), logRequests=True)
-    server.register_instance(workersManagerFunctions.WorkersManagerFunctions())
 
-    #name_thread = threading.Thread(target=treat_ping_master)
+    #name_thread = threading.Thread(target=treat_ping_master, args=())
 else:
     node="worker"
     
@@ -49,9 +45,10 @@ else:
     proxy.addWorker('http://localhost:'+sys.argv[1])
 
     server = SimpleXMLRPCServer(('localhost', int(sys.argv[1])), logRequests=True)
-    server.register_instance(clientFunctions.ClientFunctions())
 
-    #name_thread = threading.Thread(target=treat_ping_worker, args=(server))
+    #name_thread = threading.Thread(target=treat_ping_worker, args=(masterManager, server))
+
+server.register_instance(workerFunctions.WorkerFunctions())
 
 try:
     print('Use control + c to exit the '+node+' node.')
--- Task2/DirectCommunicationXMLRPC/workerFunctions.py
+++ Task2/DirectCommunicationXMLRPC/workerFunctions.py
@@ -1,6 +1,23 @@
+import redis
 import pandas
 
-class ClientFunctions:
+class WorkerFunctions:
+    redis_cli = redis.Redis(host="localhost", port=16379, decode_responses=True, encoding="utf-8")
+    
+    def addWorker(self, worker):
+        self.redis_cli.rpush('workers', worker)
+        return ' '
+
+    def getWorker(self):
+        return self.redis_cli.lpop('workers')
+
+    def numWorkers(self):
+        return self.redis_cli.llen('workers')
+
+    def removeWorker(self, port):
+        self.redis_cli.lrem('workers', 0, 'http://localhost:'+port)
+        return ' '
+        
     def readCSV(self, name_file):
         self.df = pandas.read_csv(name_file)
         return str(self.df)
