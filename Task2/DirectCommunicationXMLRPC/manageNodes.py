83f2fa0f57780aea9c8cc3b4d9ee13e667530996
Arey Ferrero Ramos <arey.ferrero2@estudiants.urv.cat>
2022-12-30 23:26:47 +0100

    Arey: Optimización de código en los detectores de fallos. Versión definitiva de la práctica.

--- Task2/DirectCommunicationXMLRPC/manageNodes.py
+++ Task2/DirectCommunicationXMLRPC/manageNodes.py
@@ -2,10 +2,8 @@ import xmlrpc.client
 import time
 
 def pingNodes(node, port, event):
-    if (node.getNodeType() == "master"):
-        proxy_master = xmlrpc.client.ServerProxy('http://localhost:'+port, allow_none=True)
-    else:
-        proxy_worker = xmlrpc.client.ServerProxy('http://localhost:'+port, allow_none=True)
+    proxy = xmlrpc.client.ServerProxy('http://localhost:'+port, allow_none=True)
+    if (node.getNodeType() == "worker"):
         workers=[]
 
     while True:
@@ -13,28 +11,28 @@ def pingNodes(node, port, event):
             break
         else:
             if (node.getNodeType() == "master"):
-                for worker in proxy_master.listWorkers():
+                for worker in proxy.listWorkers():
                     try:
                         xmlrpc.client.ServerProxy(worker, allow_none=True).isAlive()
                     except:
-                        proxy_master.removeWorker(worker.split(':')[2])
+                        proxy.removeWorker(worker.split(':')[2])
             else:
                 try:
-                    proxy_master = xmlrpc.client.ServerProxy(proxy_worker.getMaster(), allow_none=True)
+                    proxy_master = xmlrpc.client.ServerProxy(proxy.getMaster(), allow_none=True)
                     proxy_master.isAlive()
                     workers = proxy_master.listWorkers()
                 except:
                     for worker in workers:
-                        proxy = xmlrpc.client.ServerProxy(worker, allow_none=True)
-                        isLeader = proxy.canBeLeader(worker.split(':')[2], port)
+                        proxy_worker = xmlrpc.client.ServerProxy(worker, allow_none=True)
+                        isLeader = proxy_worker.canBeLeader(worker.split(':')[2], port)
                         if (isLeader == False):
                             break
                     if (isLeader == True):
                         node.setNodeType("master")
                         workers.remove('http://localhost:'+port)
-                        proxy_master = xmlrpc.client.ServerProxy('http://localhost:'+port, allow_none=True)
+                        proxy = xmlrpc.client.ServerProxy('http://localhost:'+port, allow_none=True)
                         for worker in workers:
-                            proxy_master.addWorker(worker)
+                            proxy.addWorker(worker)
                             proxy_worker = xmlrpc.client.ServerProxy(worker, allow_none=False)
                             proxy_worker.setMaster('http://localhost:'+port)
         time.sleep(1)
